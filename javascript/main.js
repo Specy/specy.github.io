accb0d04fc712ddfbe6ffb47beb669e3729b41c5
Specy <dionea2002@gmail.com>
2020-08-19 17:38:07 +0200

    Almost final changes, fixed some css, commented code, added features

--- javascript/main.js
+++ javascript/main.js
@@ -8,6 +8,8 @@ let screenWidth = positionInfo.width
 let screenHeight = positionInfo.height
 let height = Math.floor(screenHeight / 1.5)
 let width = Math.floor(screenWidth / 1.5)
+
+//limiting the matrix size to increase performance
 if (screenWidth > 1400) {
     height = Math.floor(screenHeight / 1.8)
     width = Math.floor(screenWidth / 1.8)
@@ -18,6 +20,7 @@ canvas2.height = height
 canvas2.width = width
 
 function createMatrix() {
+    //function that creates a matrix, in which there will be stored the alive/dead cells
     let matrix = []
     for (let i = 0; i < height; i++) {
         let row = new Uint8Array(width)
@@ -25,9 +28,11 @@ function createMatrix() {
     }
     return matrix
 }
+//main matrix, this will store all the current generation data that will be drawn
 let matrix = createMatrix()
 
 function redirect(url, samePage) {
+    //function to redirect to a new URL 
     if (samePage) {
         location.href = window.location.origin + url
     } else {
@@ -36,13 +41,15 @@ function redirect(url, samePage) {
 }
 
 function hexToRgb(hex) {
+    //converts the hex color to RGB, this is then used in the drawing of the canvas
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
+    return {
         r: parseInt(result[1], 16),
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
-    } : null;
+    }
 }
+//buffer element that stores the second canvas colors, initiates to being fully black 
 let secondCanvasData = new Uint8ClampedArray(width * height * 4).fill(0)
 
 function drawCanvas(toDraw, context, color, erase) {
@@ -56,19 +63,25 @@ function drawCanvas(toDraw, context, color, erase) {
     } else {
         data = secondCanvasData
     }
+    //the counter is the individual pixel position in the image, divided in 4, r g b a, 
     let counter = 0
     for (let i = 0; i < drawHeight; i++) {
         for (let j = 0; j < drawWidth; j++) {
+            //if the current pixel has never been drawn
             if (data[counter + 3]) {
+                //if it has, ignore it and continue
                 counter += 4
             } else {
+                //if it's not, add each subpixel data
                 if (toDraw[i][j]) {
+                    //if the cell is alive add the selected color to the data
                     data[counter++] = rgb[0]
                     data[counter++] = rgb[1]
                     data[counter++] = rgb[2]
                     data[counter++] = 255
 
                 } else {
+                    //if the cell is dead, turn the pixel black
                     counter += 3
                     data[counter++] = 0
                 }
@@ -76,14 +89,17 @@ function drawCanvas(toDraw, context, color, erase) {
         }
     }
     if (!erase) secondCanvasData = data
+    //actually draw the image generated above
     context.putImageData(new ImageData(data, width, height), 0, 0)
 }
 
 function eraseCanvas(context) {
+    //function to erase the canvas
     context.clearRect(0, 0, width, height);
 }
 
 function generateRandomMatrix(bias) {
+    //function that generates random cells in the matrix, the bias increases/decreases the amounts of cells created
     for (let i = 0; i < height; i++) {
         for (let j = 0; j < width; j++) {
             if (!matrix[i][j]) matrix[i][j] = (Math.round(Math.random() - bias))
@@ -92,6 +108,7 @@ function generateRandomMatrix(bias) {
     return matrix
 }
 document.getElementById("mainContent").addEventListener("touchmove", event => {
+    //listen to the touches on the screen and add the selected cells to the matrix
     let x = Math.floor((event.touches[0].pageX / screenWidth) * width)
     let y = Math.floor(((event.touches[0].pageY - window.scrollY) / screenHeight) * height)
     let noise = Math.round(Math.random() * 10 + 30)
@@ -99,9 +116,9 @@ document.getElementById("mainContent").addEventListener("touchmove", event => {
 })
 
 
-const delay = ms => new Promise(res => setTimeout(res, ms))
 window.addEventListener("mousemove", event => {
-    //if (!canDraw) return
+    //listen to the mouse moves on the screen and add the selected cells to the matrix
+    if (!canDraw) return
     let x = Math.floor((event.pageX / screenWidth) * width)
     let y = Math.floor(((event.pageY - window.scrollY) / screenHeight) * height)
     let noise = Math.round(Math.random() * 10 + 30)
@@ -110,6 +127,7 @@ window.addEventListener("mousemove", event => {
 
 
 function drawMatrix(x, y, noise) {
+    //function that adds alive cells to the matrix, the noise is the amount of cells to add which are randomly positioned
     try {
         for (let i = 0; i < noise; i++) {
             let noiseX = Math.round(Math.random() * -20 + 15)
@@ -120,11 +138,13 @@ function drawMatrix(x, y, noise) {
 }
 
 function calculateGeneration() {
+    //function that calculates the next generation for Conway's game of life
     let nextGen = createMatrix()
     let width = nextGen[0].length - 1
     let height = nextGen.length - 1
     for (let i = 1; i < height; i++) {
         for (let j = 1; j < width; j++) {
+            //checks for every neighbour, 1 is alive, 0 is dead
             let neighbours = matrix[i - 1][j - 1] +
                 matrix[i - 1][j] +
                 matrix[i - 1][j + 1] +
@@ -150,7 +170,6 @@ function calculateGeneration() {
     }
     return nextGen
 }
-drawCanvas(matrix, ctx, "#DA0363", true)
 
 let palette = {
     0: ['#283049', '#404B69', '#278EA5'],
@@ -161,6 +180,7 @@ let palette = {
 }
 
 function getRandomColor() {
+    //function that returns a random color from the palette
     return palette[0][Math.floor(Math.random() * palette[0].length)]
 }
 let every30 = 0
@@ -168,19 +188,24 @@ let secondContextColor = getRandomColor()
 let generations = []
 
 function handleFrame() {
+    //function that handles each frame, it clears the canvas and redraws on top of it
     eraseCanvas(ctx)
     if (isStopped) {
+        //if it's stopped, continue to render but don't calculate the next generations
         drawCanvas(matrix, ctx, "#DA0363", true)
     } else {
         let nextGen = calculateGeneration()
         every30++
         if (every30 > 30) {
+            //change color every 30 frames
             secondContextColor = getRandomColor()
             every30 = 0
         }
+        //adds each generation to an array, this is made to have some space between the current generation and the next one
         if (generations.length > 15) generations.shift()
         generations.push(nextGen)
         if (trailToggled) {
+            //if the trail is toggled, draw on the second canvas the trail
             drawCanvas(generations[0], ctx2, secondContextColor, false)
         }
         drawCanvas(nextGen, ctx, "#DA0363", true)
@@ -193,6 +218,7 @@ function handleFrame() {
 let trailToggled = false
 
 function toggleTrail() {
+    //function to toggle the trail animation
     trailToggled = !trailToggled
     let btn = document.getElementById("trailBtn")
     if (trailToggled) {
@@ -202,9 +228,10 @@ function toggleTrail() {
     }
 }
 
-isStopped = false
+let isStopped = false
 
 function stop() {
+    //function that stops/plays the calculation of the generation 
     isStopped = !isStopped
     let stopButton = document.getElementById("stopBtn")
     if (isStopped) {
@@ -214,7 +241,8 @@ function stop() {
     }
 }
 document.getElementById("mainContent").addEventListener("scroll", function () {
-    let blur = ((this.scrollTop - screenHeight + 200) / 200).toFixed(2)
+    //when scrolling down the page, if it reached the "about me", start blurring the canvas to make it easier to see the text
+    let blur = ((this.scrollTop - screenHeight + 100) / 200).toFixed(2)
     if (blur > 1.5) return
     canvas.style.filter = 'blur(' + blur + 'px)'
     canvas2.style.filter = 'blur(' + blur + 'px)'
@@ -225,6 +253,7 @@ function random() {
 }
 
 function erase() {
+    //function to erase the canvas and the arrays
     matrix = createMatrix()
     generations = []
     secondCanvasData = new Uint8ClampedArray(width * height * 4)
@@ -233,7 +262,7 @@ function erase() {
 }
 
 async function showHiddenDiv(div) {
-
+    //hides all the cells information and displays the current one
     let toHide = div.getElementsByClassName("hiddenDiv")[0]
     if (toHide.style.display != "block") {
         $(".hiddenDiv").css({
@@ -245,6 +274,7 @@ async function showHiddenDiv(div) {
 }
 
 function goToElement(element, scroll = 0.9) {
+    //function to scroll the body to a selected element, scroll is the offset
     let container = document.getElementById("mainContent")
     if (typeof element == "string") element = document.getElementById(element)
     $(container).animate({
@@ -256,51 +286,78 @@ function toggleUtils() {
     let utils = document.getElementById("utils")
     utils.classList.toggle("hide")
 }
-window.requestAnimationFrame(handleFrame)
 
 let fps = 0
-let lastCalledTime = 0
+let lastTime = 0
 
 function calcFps() {
-    if (!lastCalledTime) {
-        lastCalledTime = Date.now();
+    //function to calculate the current fps of the canvas, mainly for debugging
+    if (!lastTime) {
+        lastTime = Date.now();
         fps = 0;
         return;
     }
-    delta = (Date.now() - lastCalledTime) / 1000;
-    lastCalledTime = Date.now();
+    let delta = (Date.now() - lastTime) / 1000;
+    lastTime = Date.now();
     fps = Math.floor(1 / delta)
 }
 setInterval(() => {
     document.getElementById("fps").innerHTML = fps
 }, 400);
 
+let canDraw = false
 async function drawS() {
+    //function to draw the S when the page loads
     let drawing
+    //fetches the array containing the positions to draw
     await fetch("/drawing.json").then(file => file.json()).then(file => drawing = file)
     toggleTrail()
     for (let i = 0; i < drawing.length; i++) {
+        //iterates through all the points and gives an offset accordingly if the device is a mobile phone or pc
+        //positions are in percentage
         let pos = drawing[i]
         let x = Math.floor((pos[0] - 7) / 100 * width)
         if (screenWidth > screenHeight) {
             x = Math.floor((pos[0] / 3 + 30) / 100 * width)
             pos[1] = +pos[1] * 1.2 - 6
         }
-        pos[1] = +pos[1] + 3
+        pos[1] = +pos[1] + 7
         let y = Math.floor(pos[1] / 100 * height)
         let noise = Math.round(Math.random() * 10 + 30)
         await delay(4)
         drawMatrix(x, y, noise)
     }
+    canDraw = true
     setTimeout(() => {
         if (trailToggled) toggleTrail()
-    }, 10000);
+    }, 3000);
 }
 drawS()
 
 function download() {
+    //function to download the canvas data to a png
     var link = document.createElement('a');
     link.download = 'CGOLdrawing.png';
     link.href = document.getElementById('canvas').toDataURL()
     link.click();
 }
+
+function bounceArrow() {
+    //function that makes the "scroll down" icon bounce
+    for (i = 0; i < 15; i++) {
+        let bounce = "5%"
+        if (screenWidth > screenHeight) bounce = "2%"
+        $("#scrollDown").animate({
+                marginTop: '-=' + bounce,
+                marginBottom: '+=' + bounce
+            }, 500)
+            .animate({
+                marginTop: '+=' + bounce,
+                marginBottom: '-=' + bounce
+            }, 500);
+    }
+}
+bounceArrow()
+drawCanvas(matrix, ctx, "#DA0363", true)
+window.requestAnimationFrame(handleFrame)
+const delay = ms => new Promise(res => setTimeout(res, ms))
