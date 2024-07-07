de469ab983b45ab3c6985393bb4c0dafd364ef68
Specy <specy.dev@gmail.com>
2024-07-07 13:05:13 +0200

    update sky/genshin music nightly

--- skyMusic/_next/static/chunks/pages/blog/posts/easyplay-1s-3b3a0012a76ddd20.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3721],{12173:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/posts/easyplay-1s",function(){return u(12625)}])}},function(n){n.O(0,[2888,9774,179],function(){return n(n.s=12173)}),_N_E=n.O()}]);
